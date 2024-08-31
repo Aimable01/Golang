c5e30ee4b0213a0fd459e177f3a206ecb1502caf
Aimable01 <aimable.kwizera14@gmail.com>
2024-08-31 21:25:48 +0200

    feat:user input and loops

--- tutorial/main.go
+++ tutorial/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 
@@ -8,6 +11,7 @@ func main() {
 	conferenceName := "Go Conference"
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50
+	bookings := []string{}
 
 	// print the variable types
 	fmt.Printf("Conference tickets is %T, remaining tickets is %T, conference name is %T \n", conferenceTickets, remainingTickets, conferenceName)
@@ -17,11 +21,40 @@ func main() {
 	fmt.Println("Get your tickets here to attend")
 
 	// data types
-	var userName string
-	var userTickets int
-	
-	userName = "Tom"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets. \n", userName,userTickets)
 
+	for{
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+
+		// ask user input
+		fmt.Printf("Enter your first name: ")
+		fmt.Scan(&firstName)
+
+		fmt.Printf("Enter your last name: ")
+		fmt.Scan(&lastName)
+
+		fmt.Printf("Enter your email: ")
+		fmt.Scan(&email)
+
+		fmt.Printf("Enter your number of tickets: ")
+		fmt.Scan(&userTickets)
+
+		// update variables
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstName + " " + lastName)
+
+
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v \n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v \n", remainingTickets, conferenceName)
+
+		firstNames := []string{}
+		for _, booking := range bookings{
+			names := strings.Fields(booking)
+			firstNames = append(firstNames,names[0])
+		} 
+		fmt.Println("The first names of bookings are: ", firstNames)
+
+	}
 }
