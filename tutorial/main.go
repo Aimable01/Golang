b71f60f365b93fa5fec6e861291864fc4e357815
Aimable01 <aimable.kwizera14@gmail.com>
2024-08-31 21:44:22 +0200

    feat: loops && conditionals

--- tutorial/main.go
+++ tutorial/main.go
@@ -22,7 +22,7 @@ func main() {
 
 	// data types
 
-	for{
+	for {
 		var firstName string
 		var lastName string
 		var email string
@@ -42,19 +42,29 @@ func main() {
 		fmt.Scan(&userTickets)
 
 		// update variables
-		remainingTickets = remainingTickets - userTickets
-		bookings = append(bookings, firstName + " " + lastName)
+		if userTickets <= remainingTickets {
+			remainingTickets = remainingTickets - userTickets
+			bookings = append(bookings, firstName + " " + lastName)
 
 
-		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v \n", firstName, lastName, userTickets, email)
-		fmt.Printf("%v tickets remaining for %v \n", remainingTickets, conferenceName)
+			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v \n", firstName, lastName, userTickets, email)
+			fmt.Printf("%v tickets remaining for %v \n", remainingTickets, conferenceName)
 
-		firstNames := []string{}
-		for _, booking := range bookings{
-			names := strings.Fields(booking)
-			firstNames = append(firstNames,names[0])
-		} 
-		fmt.Println("The first names of bookings are: ", firstNames)
+			firstNames := []string{}
+			for _, booking := range bookings{
+				names := strings.Fields(booking)
+				firstNames = append(firstNames,names[0])
+			} 
+			fmt.Println("The first names of bookings are: ", firstNames)
 
+			// check for the remaining tickets
+			if remainingTickets == 0 {
+				fmt.Println("Our conference is booked out. Come back next year")
+				break
+			}
+			
+		} else {
+			fmt.Printf("We have only %v tickets remaining, so you can't book %v tickets. \n", remainingTickets, userTickets)
+		}		
 	}
 }
