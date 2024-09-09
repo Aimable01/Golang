d8ff3b00b0fe7dd311beeffec36e1e157aa8a4e0
Aimable01 <aimable.kwizera14@gmail.com>
2024-09-09 13:38:48 +0200

    feat: render html tmpl

--- snippetbox/cmd/web/handlers.go
+++ snippetbox/cmd/web/handlers.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
@@ -11,6 +13,21 @@ func home(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w,r)
 		return
 	}
+
+	ts,err := template.ParseFiles("./ui/html/home.page.tmpl")
+
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.Execute(w,nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+	}
+	
 	w.Write([]byte("Hello from snippetbox"))
 }
 
