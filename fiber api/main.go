ee0b7499a9366903b1a14f479afb5d474a86d65b
Aimable01 <aimable.kwizera14@gmail.com>
2024-09-11 06:32:32 +0200

    init

---
+++ fiber api/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Use(logger.New())
+	app.Use(cors.New())
+
+	app.Use(func(c *fiber.Ctx) error {
+		return c.SendStatus(404)
+	})
+
+	app.Listen(":3000")
+}
