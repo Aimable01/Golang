cc422587af2bfa26c171e00233061625f2682424
Aimable01 <aimable.kwizera14@gmail.com>
2024-11-16 16:33:26 +0200

    feat: graphql hello world

--- graphql/go.mod
+++ graphql/go.mod
@@ -1,3 +1,5 @@
 module github.com/aimable01/gql
 
 go 1.23.2
+
+require github.com/graphql-go/graphql v0.8.1
--- graphql/main.go
+++ graphql/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/graphql-go/graphql"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("GraphQl learn")
+
+	fields := graphql.Fields{
+		"hello": &graphql.Field{
+			Type: graphql.String,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return "world", nil
+			},
+		},
+	}
+
+	// defines the object config
+	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
+	// defines the schema config
+	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
+	// creates or schema
+	schema, err := graphql.NewSchema(schemaConfig)
+	if err != nil {
+		log.Fatalf("failed to create new schema, error: %v", err)
+	}
+
+	query := `
+		{
+			hello
+		}
+	`
+	params := graphql.Params{Schema: schema, RequestString: query}
+	r := graphql.Do(params)
+	if len(r.Errors) > 0 {
+		log.Fatalf("Failed to execute grapqhl operation, errors: %+v", r.Errors)
+	}
+	rJSON, _ := json.Marshal(r)
+	fmt.Printf("%s \n", rJSON)
 }
