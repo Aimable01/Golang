6fddd875c8e33db3c66aac8bacc6e525ac2f2d9b
Aimable01 <aimable.kwizera14@gmail.com>
2024-11-19 08:32:06 +0200

    feat: gql connect

--- hackerNewsGqlClient/src/main.tsx
+++ hackerNewsGqlClient/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import client from "./graphql/graphqlClient.ts";
+import { ApolloProvider } from "@apollo/client";
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </StrictMode>
+);
