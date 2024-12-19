7055f325664dc9f651c752c8c33428e5515a4b88
Aimable01 <aimable.kwizera14@gmail.com>
2024-12-19 09:35:22 +0200

    feat: auth middleware

---
+++ nest fit/server/internal/auth/middleware.go
@@ -0,0 +1,53 @@
+package auth
+
+import (
+	"context"
+	"net/http"
+
+	database "github.com/aimable01/nestfit/internal/pkg/db/postgres"
+	"github.com/aimable01/nestfit/internal/pkg/jwt"
+	"github.com/aimable01/nestfit/internal/pkg/models"
+)
+
+var userCtxKey = &contextKey{"user"}
+
+type contextKey struct {
+	name string
+}
+
+func Middleware() func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			header := r.Header.Get("Authorization")
+
+			if header == "" {
+				next.ServeHTTP(w, r)
+				return
+			}
+
+			tokenStr := header
+			userID, err := jwt.ParseToken(tokenStr)
+			if err != nil {
+				http.Error(w, "Invalid token", http.StatusForbidden)
+				return
+			}
+
+			var user models.User
+			db := database.DB
+			if err := db.First(&user, "id = ?", userID).Error; err != nil {
+				http.Error(w, "User not found", http.StatusUnauthorized)
+				return
+			}
+
+			ctx := context.WithValue(r.Context(), userCtxKey, &user)
+
+			r = r.WithContext(ctx)
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func ForContext(ctx context.Context) *models.User {
+	raw, _ := ctx.Value(userCtxKey).(*models.User)
+	return raw
+}
--- nest fit/server/internal/pkg/models/user.go
+++ nest fit/server/internal/pkg/models/user.go
@@ -2,22 +2,48 @@ package models
 
 import (
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
 type User struct {
 	gorm.Model
-	ID       uuid.UUID `gorm:"type:uuid;"`
-	Username string    `json:"username"`
-	Email    string    `json:"email"`
-	Password string    `json:"password"`
+	ID       uuid.UUID `gorm:"type:uuid;primary_key"`
+	Username string    `json:"username" gorm:"unique;not null"`
+	Email    string    `json:"email" gorm:"unique;not null"`
+	Password string    `json:"password" gorm:"not null"`
 }
 
 type Users struct {
 	Users []User `json:"users"`
 }
 
+// set id
 func (user *User) BeforeCreate(tx *gorm.DB) (err error) {
 	user.ID = uuid.New()
 	return
+}
+
+func (user *User) Create(db *gorm.DB) error {
+	// hash password
+	hashedPassword, err := HashPassword(user.Password)
+	if err != nil {
+		return err
+	}
+	user.Password = hashedPassword
+
+	if err := db.Create(user).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
