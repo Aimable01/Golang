c7867ca0ac1439845ccef823778e0c6282523b01
Aimable01 <aimable.kwizera14@gmail.com>
2025-01-05 11:01:29 +0200

    feat:[client] signup form

--- nest fit/client/src/app/auth/signup.tsx
+++ nest fit/client/src/app/auth/signup.tsx
@@ -2,23 +2,105 @@ import { useForm } from "react-hook-form";
 import { SignupInputs } from "../../types/auth";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { signupSchema } from "../../schemas/authSchema";
+import { Eye, EyeOff, Loader } from "lucide-react";
+import { useState } from "react";
 
 export default function Signup() {
-  const { handleSubmit, register } = useForm<SignupInputs>({
+  const [viewPassword, setViewPassword] = useState<boolean>(false);
+  const {
+    handleSubmit,
+    register,
+    formState: { isSubmitting, errors },
+  } = useForm<SignupInputs>({
     resolver: zodResolver(signupSchema),
   });
+
+  const submit = async (data: SignupInputs) => {
+    console.log(data);
+  };
+
   return (
-    <div className="flex flex-col gap-4 items-center justify-center min-h-screen text-white">
-      <h1 className="font-[500] text-[16px]">Create your Nest Fit account</h1>
-      <form onSubmit={handleSubmit(() => {})}>
-        <div className="flex items-center gap-2 px-2 py-3 rounded-md">
+    <div className="flex flex-col gap-4 items-center justify-center min-h-screen text-white mx-10 md:mx-20">
+      <h1 className="font-semibold text-[18px] sm:text-[20px] md:text-[22px] lg:text-[26px] text-gray-300">
+        Create your Nest Fit account
+      </h1>
+
+      <form
+        className="flex flex-col gap-3 w-full sm:w-[350px] md:w-[450px] lg:w-[500px]"
+        onSubmit={handleSubmit(submit)}
+      >
+        {/* Username Input */}
+        <div className="flex flex-col gap-1">
           <input
             type="text"
             placeholder="Username"
-            {...register("email")}
-            className="bg-gray-200"
+            {...register("username")}
+            className="bg-custom-gray w-full p-3 rounded-md focus:outline-none font-light text-[14px] sm:text-[16px]"
           />
+          {errors.username && (
+            <span className="text-red-400 text-[12px] font-medium px-2">
+              {errors.username.message}
+            </span>
+          )}
         </div>
+
+        {/* Email Input */}
+        <div className="flex flex-col gap-1">
+          <input
+            type="text"
+            placeholder="Email"
+            {...register("email")}
+            className="bg-custom-gray w-full p-3 rounded-md focus:outline-none font-light text-[14px] sm:text-[16px]"
+          />
+          {errors.email && (
+            <span className="text-red-400 text-[12px] font-medium px-2">
+              {errors.email.message}
+            </span>
+          )}
+        </div>
+
+        {/* Password Input */}
+        <div className="flex flex-col gap-1">
+          <div className="flex items-center bg-custom-gray p-3 rounded-md w-full">
+            <input
+              type={viewPassword ? "text" : "password"}
+              placeholder="Password"
+              {...register("password")}
+              className="bg-transparent w-full focus:outline-none font-light text-[14px] sm:text-[16px]"
+            />
+            <div
+              className="cursor-pointer"
+              onClick={() => setViewPassword(!viewPassword)}
+            >
+              {viewPassword ? (
+                <EyeOff className="text-gray-400" width={20} />
+              ) : (
+                <Eye className="text-gray-400" width={20} />
+              )}
+            </div>
+          </div>
+          {errors.password && (
+            <span className="text-red-400 text-[12px] font-medium px-2">
+              {errors.password.message}
+            </span>
+          )}
+        </div>
+
+        {/* Submit Button */}
+        <button
+          type="submit"
+          disabled={isSubmitting}
+          className="flex items-center justify-center gap-2 bg-gray-700 hover:bg-gray-600 text-white p-3 rounded-md transition duration-300 font-medium text-[14px] sm:text-[16px]"
+        >
+          {isSubmitting ? (
+            <>
+              <Loader className="animate-spin" size={20} />
+              Logging in...
+            </>
+          ) : (
+            "Sign up"
+          )}
+        </button>
       </form>
     </div>
   );
--- nest fit/client/tailwind.config.js
+++ nest fit/client/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-gray": "rgb(38, 38, 38)",
+      },
+    },
   },
   plugins: [],
 };
