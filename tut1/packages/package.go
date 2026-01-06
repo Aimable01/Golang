4ade61a93e0f624dab09a2e075eef7148ce5266f
Aimable01 <aimable.kwizera14@gmail.com>
2026-01-06 20:12:48 +0200

    imports & exports

--- tut1/packages/package.go
+++ tut1/packages/package.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 )
 
 func main() {
 	fmt.Printf("My favorite number is: %d", rand.Intn(10)) // this will return a random number between (0,10)
+
+	fmt.Println(math.Pi) // all exported names should begin with a capital letter
 }
